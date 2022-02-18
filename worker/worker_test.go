fdfb3268df2b52c99b55158433c0d2ab0063047c
Alan Cesar Elias <alan.elias@bexsbanco.com.br>
2022-02-18 14:13:18 -0300

    Add unit tests

---
+++ worker/worker_test.go
@@ -0,0 +1,58 @@
+package worker
+
+import (
+	"context"
+	"math/rand"
+	"reflect"
+	"sync/atomic"
+	"testing"
+	"time"
+)
+
+func TestWorker_Work(t *testing.T) {
+	type fields struct {
+		fn     func(counter *uint64) Fn
+		buffer int
+	}
+	type args struct {
+		ctx     context.Context
+		items   []interface{}
+		counter uint64
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   uint64
+	}{
+		{
+			name: "Should execute all jobs properly",
+			fields: fields{
+				fn: func(counter *uint64) Fn {
+					return func(ctx context.Context, input interface{}) {
+						atomic.AddUint64(counter, 1)
+						time.Sleep(time.Millisecond * time.Duration(input.(int)) * 100)
+					}
+				},
+				buffer: 2,
+			},
+			args: args{
+				ctx:     context.Background(),
+				items:   []interface{}{rand.Intn(10), rand.Intn(10), rand.Intn(10), rand.Intn(10), rand.Intn(10)},
+				counter: 0,
+			},
+			want: 5,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			counter := tt.args.counter
+			w := New(tt.fields.fn(&counter), tt.fields.buffer)
+			w.Work(tt.args.ctx, tt.args.items)
+
+			if !reflect.DeepEqual(counter, tt.want) {
+				t.Errorf("Work() = %v, want %v", tt.args.counter, tt.want)
+			}
+		})
+	}
+}
