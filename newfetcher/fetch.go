25fea6f7bd94bf2672c633724ba1de29943c3306
Alan Cesar <alancesarelias@gmail.com>
2022-02-19 18:44:45 -0300

    Initial fetcher implementation

---
+++ newfetcher/fetch.go
@@ -0,0 +1,25 @@
+package newfetcher
+
+import "sync"
+
+type (
+	ProviderFn[T any]                func() (T, error)
+	ProviderWithParamFn[T, K any]    func(T) (K, error)
+	CallbackFn[T sync.Locker, S any] func(target T, source S) error
+)
+
+func Fetcher[T sync.Locker, K any](target T, provider ProviderFn[K], callback CallbackFn[T, K]) error {
+	defer target.Unlock()
+	source, err := provider()
+	if err != nil {
+		return err
+	}
+	target.Lock()
+	return callback(target, source)
+}
+
+func FetcherWithParam[T sync.Locker, P any, S any](target T, param P, provider ProviderWithParamFn[P, S], callback CallbackFn[T, S]) error {
+	return Fetcher(target, func() (S, error) {
+		return provider(param)
+	}, callback)
+}
