612baf9a526c0738859e363a9e99d93e83bf6204
Alan Cesar <alancesarelias@gmail.com>
2022-02-20 14:42:54 -0300

    Rename package

--- fetcher/fetch_test.go
+++ fetcher/fetch_test.go
@@ -14,8 +14,8 @@ type testData struct {
 
 func TestNew(t *testing.T) {
 	type args struct {
-		provider ProviderFn
-		callback CallbackFn
+		provider ProviderFn[string]
+		callback CallbackFn[*testData, string]
 	}
 	tests := []struct {
 		name    string
@@ -26,11 +26,11 @@ func TestNew(t *testing.T) {
 		{
 			name: "Should fetch all properties properly",
 			args: args{
-				provider: func() (interface{}, error) {
+				provider: func() (string, error) {
 					return "Some value", nil
 				},
-				callback: func(target sync.Locker, source interface{}) error {
-					target.(*testData).Value = source.(string)
+				callback: func(target *testData, source string) error {
+					target.Value = source
 					return nil
 				},
 			},
@@ -46,7 +46,7 @@ func TestNew(t *testing.T) {
 			target := testData{}
 
 			group.Go(func() error {
-				return New(&target, tt.args.provider, tt.args.callback)
+				return New[*testData](&target, tt.args.provider, tt.args.callback)
 			})
 
 			if err := group.Wait(); (err != nil) != tt.wantErr {
